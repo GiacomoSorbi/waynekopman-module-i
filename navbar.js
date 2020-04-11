377aa2093526b7a576b5f42f511a96b547376a69
Wayne Kopman <56189380+WayneKopman@users.noreply.github.com>
2020-04-11 23:51:17 +0100

    Basket styling
    Re-style basket. Burger menu fix.

--- navbar.js
+++ navbar.js
@@ -89,8 +89,8 @@ function addItemToCart(title, price, imageSrc) {
 }
     let cartRowContents = `
     <div class="cart-item cart-column">
-    <img class="cart-item-image" src="${imageSrc}" width="100" height="100">
     <span class="cart-item-title">${title}</span>
+    <img class="cart-item-image" src="${imageSrc}" width="100" height="100">
     </div><span class="cart-price cart-column">${price}</span>
     <div class="cart-quantity cart-column">
     <input class="cart-quantity-input" type="number" value="1">
